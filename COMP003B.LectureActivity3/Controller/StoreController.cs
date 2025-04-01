dbdb45d9991e3d18efdbf2fd6f70ec07028c950e
romanvillagran <romanvillagran386@gmail.com>
2025-03-31 18:44:46 -0700

    added everything to the store controller

---
+++ COMP003B.LectureActivity3/Controller/StoreController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace COMP003B.LectureActivity3.Controllers
+{
+    [Route("store")]
+
+    public class StoreController : Controller
+    {
+        [Route("product/{id:int}")]
+
+        public IActionResult ViewProduct(int id)
+        {
+            return View(id);
+        }
+        
+        
+        [HttpGet("Product/search")]
+
+        public IActionResult Search([FromQuery] string category)
+        {
+            //the view() method returns a view result that renders a view to the response
+            //the category parameter is passed to the view
+            //the category parameter is retrieved from the query string
+            //the [fromquery] attridute specifies that the peremter should be bound using the query string
+            return View("search", category);
+        }
+    }
+}
