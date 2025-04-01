6a9d53625a14da07435d2aa1c99aadf9783b05cf
romanvillagran <romanvillagran386@gmail.com>
2025-03-31 18:36:59 -0700

    added everthing in product controller

---
+++ COMP003B.LectureActivity3/Controller/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using COMP003B.LectureActivity3.Model;
+
+namespace COMP003B.LectureActivity3.Controllers
+{
+    [Route("products")]
+
+    public class ProductController : Controller
+    {
+        [HttpGet("create")]
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost("create")]
+        public IActionResult Create([FromForm] Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(product);
+            }
+
+            return RedirectToAction("Success", product);
+        }
+
+        [Route("Success")]
+        public IActionResult Success(Product product)
+        {
+            return View(product);
+        }
+   
+    }
+}
