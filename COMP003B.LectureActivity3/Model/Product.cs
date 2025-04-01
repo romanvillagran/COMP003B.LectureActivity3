62e4bfe22c83133d7aeb267e6286135d6e114f0c
romanvillagran <romanvillagran386@gmail.com>
2025-03-31 18:19:02 -0700

    added everything to product.cs

---
+++ COMP003B.LectureActivity3/Model/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.LectureActivity3.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Range(0.01, 10000)]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string Catagory { get; set; }
+    }
+}
